0d3d6f5c8aac44269a0c6d6c6277bb74e7ba5104
Alex-Brems <abrems13@yahoo.com>
2018-11-11 13:14:39 -0700

    11/11/2018 Project 33 Complete

---
+++ project/33/test.php
@@ -0,0 +1,33 @@
+<?php
+
+    require_once 'views.php'; 
+    require_once 'log.php';
+    require_once 'notes_data.php';
+    
+
+    // Page content
+
+    $text = 'Experiment with page rendering, before proceeding to the next steps.';
+
+    $preform = '<pre> function render() { do_this(); }</pre>';
+
+    $markdown = render_markdown("* Bullet lists\n* Styles\n* Headings");
+
+    $notes = note_list_view(query_notes());
+        
+    $content = 
+        render_card('Page Rendering', $text) .
+        render_card('Preformatted Text', $preform) .
+        render_card('Markdown', $markdown) . 
+        render_card('Notes', $notes);
+
+
+    // Create main part of page content
+    $settings = array(
+        "site_title" => "Exterior Brain",
+        "page_title" => "Test of Dev Features", 
+        "content"    => $content);
+
+    echo render_page($settings);
+
+?>
